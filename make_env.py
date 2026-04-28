b627b91cb4aa203f883df035b4cb1e42650e69a5
tranh1302 <trunganh1302tranh@gmail.com>
2026-04-28 15:31:33 +0700

    Add parameters for environment configuration

--- make_env.py
+++ make_env.py
@@ -2,7 +2,14 @@ import gymnasium as gym
 from config import env_id, num_envs
 
 def make_env(env_id, capture_video):
-  env = gym.make(env_id, render_mode = 'rgb_array')
+  env = gym.make(
+    env_id,
+    render_mode = 'rgb_array',
+    forward_reward_weight=1.75,
+    healthy_reward=5.0,
+    ctrl_cost_weight=0.1,
+    terminate_when_unhealthy=True
+  )
   if capture_video:
     env = gym.wrappers.RecordVideo(env, 'video')
   env = gym.wrappers.RecordEpisodeStatistics(env)
